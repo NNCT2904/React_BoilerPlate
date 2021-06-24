d50e909ba19cd633a078aac1d9e3d55e1d60de25
Thanh Nguyen <nnct2000@gmail.com>
2021-06-24 10:45:40 +1000

    Add API for insult

--- backend/src/app/Server.ts
+++ backend/src/app/Server.ts
@@ -1,8 +1,14 @@
 import { Express, Request, Response } from 'express';
+import randomInsult from '../features/insult';
 
 const server = (app: Express ,port: number) => {
     app.get("/api", (req: Request, res: Response): void => {
         res.send("Backend API running!");
+    });
+
+    app.get("/api/insult", (req:Request, res: Response): void => {
+        const insult = randomInsult();
+        res.send(insult);
     });
 
     app.listen(port, () => console.log(`Server is running on port ${port}`));
