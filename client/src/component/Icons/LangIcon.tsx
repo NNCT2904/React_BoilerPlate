fbc8c33cbce4d2090b5a0a19d893a52082d8ac59
ThanhNguyen <nnct2000@gmail.com>
2021-08-25 16:11:54 +1000

    Added Icon components

---
+++ client/src/component/Icons/LangIcon.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconPrefix, IconName } from '@fortawesome/fontawesome-svg-core';
+
+interface LangIconProps {
+  name: string;
+}
+const LangIcon: FC<LangIconProps> = ({ name }) => {
+  const iconProps = ['fab', name] as [IconPrefix, IconName];
+
+  return (
+    <li>
+      <FontAwesomeIcon icon={iconProps} />
+    </li>
+  );
+};
+
+export default LangIcon;
